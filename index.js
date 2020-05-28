145b45d0f21aaa88818b62e994ec6f9c911f1807
Emanuel Mazilu <emanuel.mazilu@outlook.com>
2020-05-28 20:52:46 +0300

    added some comments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// function that executes when the chanel link is clicked
+// change the source for the video player and reload
+
 function changeChannel(e){
     event.preventDefault();
     var link = e.target.href;
