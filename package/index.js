74feb7ebac3e3b75d07f76570228cb20ec166039
Emanuel Mazilu <emanuel.mazilu@outlook.com>
2020-05-29 07:46:36 +0300

    clean up

--- package/index.js
+++
@@ -1,9 +0,0 @@
-// function that executes when the chanel link is clicked
-// change the source for the video player and reload
-
-function changeChannel(e){
-    event.preventDefault();
-    var link = e.target.href;
-    var videoPlayer = videojs("my-video");
-    videoPlayer.src({src: link})
-}
